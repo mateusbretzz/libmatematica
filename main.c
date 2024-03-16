c39ff0ec8a2c34f0916900236b2a6aa5ecf1f894
mateusbretzz <teubr2202@gmail.com>
2024-03-16 17:18:58 -0300

    Colocando angulo em radianos

--- main.c
+++ main.c
@@ -2,9 +2,9 @@
 #include "matematica.h"
 
 int main() {
-    float angulo = 45;
+    float angulo = 3.14/4;
     Trigonometria valor = calcular_trigonometria(angulo);
-
+    
     printf("O seno de %.f: %f\n", angulo, valor.seno);
     printf("O cosseno de %.f: %f\n", angulo, valor.cosseno);
     printf("A tangente de %.f: %f\n", angulo, valor.tangente);
